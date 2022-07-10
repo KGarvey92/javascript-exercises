ef9abb3ac0ed80108dbaaa2fbf9f300de1397040
KGarvey92 <kgarvey92@gmail.com>
2022-07-10 20:33:53 +0800

    all tests complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) { 
+  let celcius = (temperature - 32) * (5 / 9);
+  return Number(celcius.toFixed(1))
 };
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+  let fahrenheit = temperature * (9 / 5) + 32;
+  return Number(fahrenheit.toFixed(1))
 };
 
 // Do not edit below this line

eb885fd033ff92c39f8d024413088858c378c3f5
KGarvey92 <kgarvey92@gmail.com>
2022-07-10 14:59:59 +0800

    pass first sumAll test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    // We can use Gauss' equation for this problem.
+    // First, find the length of the sequence of numbers.
+    let n = Math.abs(a - b) + 1;
+    let sum = (n / 2) * (a + b);
+    return sum;
 };
 
 // Do not edit below this line

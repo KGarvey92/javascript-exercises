73e1e9313b3c6bbcae64236936cdab633d395c4b
KGarvey92 <kgarvey92@gmail.com>
2022-08-10 19:17:01 +0800

    finish calculator and palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    //strip away punctuation, all empty space and convert to lowercase
+    strippedString = [];
+    for (let i = 0; i < string.length; i++) {
+        if (string[i].toLowerCase() != string[i].toUpperCase()) {
+            strippedString.push(string[i].toLowerCase());
+        }
+    }
+    console.log(strippedString);
+    // reverse string and compare.
+    reverseString = strippedString.slice().reverse().join('');
+    console.log(reverseString);
+    
+    if (reverseString === strippedString.join('')) {
+        return true;
+    } else {
+        return false;
+    };
 };
 
 // Do not edit below this line

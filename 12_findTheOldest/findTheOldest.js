281d1038e24da224e2e55d7f1f1bd3ddc338119e
KGarvey92 <kgarvey92@gmail.com>
2022-08-11 12:43:05 +0800

    complete findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    // use sort to compare people one by one
+    const oldest = people.sort((a, b) => {
+        // account for those still alive
+        let aAge;
+        let bAge;
+        if (!a.yearOfDeath) {
+            aAge = new Date().getFullYear() - a.yearOfBirth;
+        }
+        else {
+            aAge = a.yearOfDeath - a.yearOfBirth;
+        }
+        if (!b.yearOfDeath) {
+            bAge = new Date().getFullYear() - b.yearOfBirth;
+        }
+        else {
+            bAge = b.yearOfDeath - b.yearOfBirth;
+        }
+        return aAge > bAge ? -1 : 1;
+    })
+    //oldest person is first index
+    return oldest[0];
 };
 
 // Do not edit below this line
